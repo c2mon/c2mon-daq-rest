e5b432886f290383794efed489bfd9f155f66f60
Matthias Braeger <Matthias.Braeger@cern.ch>
2018-05-28 13:23:48 +0200

    #12: Minor code improvement

--- src/main/java/cern/c2mon/daq/rest/scheduling/GetScheduler.java
+++ src/main/java/cern/c2mon/daq/rest/scheduling/GetScheduler.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
+ * Copyright (C) 2010-2018 CERN. All rights not expressly granted are reserved.
  *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
@@ -106,7 +106,7 @@ public class GetScheduler extends RestScheduler {
 
         // request to the web service
         String restMessage = RESTConnector.sendAndReceiveRequest(url);
-        Class dataType = TypeConverter.getType(sdt.getDataType());
+        Class<?> dataType = TypeConverter.getType(sdt.getDataType());
 
         // convert Message if jsonPathExpression is given
         if (jsonPathExpression != null) {
